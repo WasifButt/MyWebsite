6c9b5acc48b36987fec0816b3d29c5e00b39ffe8
Wasif Butt <wasifking@gmail.com>
2020-05-15 21:53:53 -0400

    Update Navbar.js

--- mywebsite/src/components/Navbar.js
+++ mywebsite/src/components/Navbar.js
@@ -15,12 +15,45 @@ const Styles = styled.div`
             color: white;
         }
     }
+
+    .sidebar {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        min-height: 100vh !important;
+        z-index: 100;
+        padding: 48px 0 0;
+        box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+    }
+    #sidebar-wrapper{
+        min-height: 100vh !important;
+        width: 100vw;
+        margin-left: -1rem;
+        -webkit-transition: margin .25s ease-out;
+        -moz-transition: margin .25s ease-out;
+        -o-transition: margin .25s ease-out;
+        transition: margin .25s ease-out;
+    }
+    #sidebar-wrapper .sidebar-heading {
+        padding: 0.875rem 1.25rem;
+        font-size: 1.2rem;
+    }
+
+    #page-content-wrapper {
+        min-width: 0;
+        width: 100%;
+    }
+
     
 `;
 
-export const Navigation = () => (
+export const Navigation = (props) => (
     <Styles>
-        <Navbar expand = "lg" sticky="top" >
+        <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
+            activeKey="/home"
+            onSelect={selectedKey => alert(`selected ${selectedKey}`)}>
+            <div className="sidebar-sticky"></div>
             <Navbar.Toggle aria-controls = "basic-navbar-nav" />
             <Navbar.Collapse id = "basic-navbar-nav">
                 <Nav className = "flex-column">
@@ -29,6 +62,6 @@ export const Navigation = () => (
                     <Nav.Item><Link to="experience" smooth = {true} duration = {1000}>My Experience</Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
-        </Navbar>
+        </Nav>
     </Styles>
 )
