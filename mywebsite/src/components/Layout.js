c10dc3bd4117eb1e92600c5189f51c08605de8a9
Wasif Butt <wasifking@gmail.com>
2020-05-15 22:04:34 -0400

    Revert "revert"
    This reverts commit 5a1411c9e3d5ed7e359acd443c0e83f5fe4dbe0b.

---
+++ mywebsite/src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Navigation } from './Navbar'
+
+export const Layout = (props) => (
+    <Container fluid>
+        <Row>
+            <Col><Navigation /></Col>
+            <Col xs = {11}>{props.children}</Col>
+        </Row>
+    </Container>
+)
